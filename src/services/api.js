b5129bf47a14c13455c4ec9727747ee6b934224e
jpoliveiramateus <joaopedromateus6@gmail.com>
2022-09-30 10:46:04 -0300

    fix: errors fetch categories

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export async function getCategories() {
     const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
     const data = await response.json();
     return data;
-  } catch {
+  } catch (e) {
     alert('Falha ao buscar categorias!');
     return categories;
   }
--- src/services/categories.js
+++ src/services/categories.js
@@ -1,130 +1,130 @@
 module.exports = [
   {
-    "id": "MLB5672",
-    "name": "Acessórios para Veículos"
+    id: "MLB5672",
+    name: "Acessórios para Veículos",
   },
   {
-    "id": "MLB271599",
-    "name": "Agro"
+    id: "MLB271599",
+    name: "Agro",
   },
   {
-    "id": "MLB1403",
-    "name": "Alimentos e Bebidas"
+    id: "MLB1403",
+    name: "Alimentos e Bebidas",
   },
   {
-    "id": "MLB1071",
-    "name": "Animais"
+    id: "MLB1071",
+    name: "Animais",
   },
   {
-    "id": "MLB1367",
-    "name": "Antiguidades e Coleções"
+    id: "MLB1367",
+    name: "Antiguidades e Coleções",
   },
   {
-    "id": "MLB1368",
-    "name": "Arte, Papelaria e Armarinho"
+    id: "MLB1368",
+    name: "Arte, Papelaria e Armarinho",
   },
   {
-    "id": "MLB1384",
-    "name": "Bebês"
+    id: "MLB1384",
+    name: "Bebês",
   },
   {
-    "id": "MLB1246",
-    "name": "Beleza e Cuidado Pessoal"
+    id: "MLB1246",
+    name: "Beleza e Cuidado Pessoal",
   },
   {
-    "id": "MLB1132",
-    "name": "Brinquedos e Hobbies"
+    id: "MLB1132",
+    name: "Brinquedos e Hobbies",
   },
   {
-    "id": "MLB1430",
-    "name": "Calçados, Roupas e Bolsas"
+    id: "MLB1430",
+    name: "Calçados, Roupas e Bolsas",
   },
   {
-    "id": "MLB1039",
-    "name": "Câmeras e Acessórios"
+    id: "MLB1039",
+    name: "Câmeras e Acessórios",
   },
   {
-    "id": "MLB1743",
-    "name": "Carros, Motos e Outros"
+    id: "MLB1743",
+    name: "Carros, Motos e Outros",
   },
   {
-    "id": "MLB1574",
-    "name": "Casa, Móveis e Decoração"
+    id: "MLB1574",
+    name: "Casa, Móveis e Decoração",
   },
   {
-    "id": "MLB1051",
-    "name": "Celulares e Telefones"
+    id: "MLB1051",
+    name: "Celulares e Telefones",
   },
   {
-    "id": "MLB1500",
-    "name": "Construção"
+    id: "MLB1500",
+    name: "Construção",
   },
   {
-    "id": "MLB5726",
-    "name": "Eletrodomésticos"
+    id: "MLB5726",
+    name: "Eletrodomésticos",
   },
   {
-    "id": "MLB1000",
-    "name": "Eletrônicos, Áudio e Vídeo"
+    id: "MLB1000",
+    name: "Eletrônicos, Áudio e Vídeo",
   },
   {
-    "id": "MLB1276",
-    "name": "Esportes e Fitness"
+    id: "MLB1276",
+    name: "Esportes e Fitness",
   },
   {
-    "id": "MLB263532",
-    "name": "Ferramentas"
+    id: "MLB263532",
+    name: "Ferramentas",
   },
   {
-    "id": "MLB12404",
-    "name": "Festas e Lembrancinhas"
+    id: "MLB12404",
+    name: "Festas e Lembrancinhas",
   },
   {
-    "id": "MLB1144",
-    "name": "Games"
+    id: "MLB1144",
+    name: "Games",
   },
   {
-    "id": "MLB1459",
-    "name": "Imóveis"
+    id: "MLB1459",
+    name: "Imóveis",
   },
   {
-    "id": "MLB1499",
-    "name": "Indústria e Comércio"
+    id: "MLB1499",
+    name: "Indústria e Comércio",
   },
   {
-    "id": "MLB1648",
-    "name": "Informática"
+    id: "MLB1648",
+    name: "Informática",
   },
   {
-    "id": "MLB218519",
-    "name": "Ingressos"
+    id: "MLB218519",
+    name: "Ingressos",
   },
   {
-    "id": "MLB1182",
-    "name": "Instrumentos Musicais"
+    id: "MLB1182",
+    name: "Instrumentos Musicais",
   },
   {
-    "id": "MLB3937",
-    "name": "Joias e Relógios"
+    id: "MLB3937",
+    name: "Joias e Relógios",
   },
   {
-    "id": "MLB1196",
-    "name": "Livros, Revistas e Comics"
+    id: "MLB1196",
+    name: "Livros, Revistas e Comics",
   },
   {
-    "id": "MLB1168",
-    "name": "Música, Filmes e Seriados"
+    id: "MLB1168",
+    name: "Música, Filmes e Seriados",
   },
   {
-    "id": "MLB264586",
-    "name": "Saúde"
+    id: "MLB264586",
+    name: "Saúde",
   },
   {
-    "id": "MLB1540",
-    "name": "Serviços"
+    id: "MLB1540",
+    name: "Serviços",
   },
   {
-    "id": "MLB1953",
-    "name": "Mais Categorias"
-  }
-]
+    id: "MLB1953",
+    name: "Mais Categorias",
+  },
+];
